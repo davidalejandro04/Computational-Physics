98dc301366e4dfee798ab1112399362f035f1bfe
davidalejandro04 <da.pabon10@uniandes.edu.co>
2019-08-16 22:54:01 -0500

    ODESympy modificado

--- ODESympy.py
+++ ODESympy.py
@@ -5,8 +5,8 @@ t,kap,w0=symbols('t kap w0')
 f(t)
 f(t).diff(t)
 
-print("\n" ODE to be solved:")
-diffeq=Eq(f(t).diff(t,t)+kap*(f(t).diff))+(w0*w0)*f(t))
+print("\n ODE to be solved:")
+diffeq=Eq(f(t).diff(t,t)+kap*(f(t).diff(t))+(w0+w0)+f(t))
 
 print(diffeq)
 print("\n Solution of ODE:")
